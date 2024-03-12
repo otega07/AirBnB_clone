78b8d0f107e883a1d49ca02eab7d82e0d7b750aa
otega07 <okiriotega@gmail.com>
2024-03-12 18:28:30 +0100

    tested

---
+++ multiply_numbers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def multiply_numbers(a: int, b: int) -> int:
+    """
+    Multiplies two numbers
+
+    Args:
+        a (int): The first number.
+        b (int): The second number.
+
+    Returns:
+        int: The product of the two numbers.
+    """
+    return a * b
